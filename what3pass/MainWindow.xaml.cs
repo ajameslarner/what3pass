3770c3aa4146da5b063c33485c47e6cb0b01d2c7
ajameslarner <ajameslarner@gmail.com>
2022-04-30 16:47:46 +0100

    Add project files.

---
+++ what3pass/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace what3pass
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txt_info_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            txt_info.Visibility = Visibility.Hidden;
+            lbl_welcome.Content = "Welcome";
+        }
+
+        private void btn_exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btn_ref_Click(object sender, RoutedEventArgs e)
+        {
+            txt_info.Visibility = Visibility.Visible;
+            txt_info.Text = "Reference details for what3words";
+            lbl_welcome.Content = "What3Words";
+        }
+
+        private void btn_lbe_Click(object sender, RoutedEventArgs e)
+        {
+            txt_info.Visibility = Visibility.Visible;
+            txt_info.Text = "Location based encryption explanation";
+            lbl_welcome.Content = "LBE";
+        }
+
+        private void btn_about_Click(object sender, RoutedEventArgs e)
+        {
+            txt_info.Visibility = Visibility.Visible;
+            txt_info.Text = "What what3pass is and a little description on how it works";
+            lbl_welcome.Content = "About";
+        }
+
+        private void btn_viewentries_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_newentry_Click(object sender, RoutedEventArgs e)
+        {
+            NewEntryWindow entryWindow = new NewEntryWindow();
+            entryWindow.Show();
+            Close();
+        }
+    }
+}
---
+++ what3pass/NewEntryWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using System.Device.Location;
+
+namespace what3pass
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class NewEntryWindow : Window
+    {
+        private double _latitude;
+        private double _longitude;
+        private GeoCoordinateWatcher _geoCoordinateWatcher;
+        private GeoCoordinate _geoCoordinate;
+        
+        public NewEntryWindow()
+        {
+            InitializeComponent();
+
+            _geoCoordinateWatcher = new GeoCoordinateWatcher();
+            _geoCoordinateWatcher.Start();
+
+            _geoCoordinateWatcher.PositionChanged += (sender, e) =>
+            {
+                _geoCoordinate = e.Position.Location;
+                _latitude = _geoCoordinate.Latitude;
+                _longitude = _geoCoordinate.Longitude;
+
+                var coordinate = e.Position.Location;
+                Console.WriteLine("Lat: {0}, Long: {1}", coordinate.Latitude,
+                    coordinate.Longitude);
+            };
+        }
+
+        private void mapView_Loaded(object sender, RoutedEventArgs e)
+        {
+            mapView.MapProvider = GoogleMapProvider.Instance;
+            GMaps.Instance.Mode = AccessMode.ServerOnly;
+            mapView.MinZoom = 2;
+            mapView.MaxZoom = 17;
+            mapView.Zoom = 2;
+            mapView.MouseWheelZoomType = MouseWheelZoomType.MousePositionAndCenter;
+            mapView.DragButton = MouseButton.Left;
+            mapView.CanDragMap = true;
+            mapView.Position = new PointLatLng(_latitude, _longitude);
+        }
+    }
+}
